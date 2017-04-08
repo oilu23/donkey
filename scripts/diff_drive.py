3465db992f5d8b7a099780f159c18a5298324b79
Julio Alvarez <oilu23@gmail.com>
2017-04-08 11:17:33 -0400

    add diff drive

--- scripts/diff_drive.py
+++ scripts/diff_drive.py
@@ -24,7 +24,11 @@ if __name__ == '__main__':
 
     cfg = dk.config.parse_config('~/mydonkey/' + args['--config'] + '.ini')
 
+    #get the url for the remote host (for user control)
     remote_url = args['--remote']
+    
+    #load config file
+    cfg = dk.config.parse_config('~/mydonkey/vehicle.ini')
 
     left_motor = dk.actuators.Adafruit_Motor_Hat_Controller(cfg['left_actuator_channel'])
     right_motor = dk.actuators.Adafruit_Motor_Hat_Controller(cfg['right_actuator_channel'])
@@ -33,13 +37,20 @@ if __name__ == '__main__':
 
     #asych img capture from picamera
     mycamera = dk.sensors.PiVideoStream()
+    
+    #setup the remote host
+    myremote = dk.remotes.RemoteClient(remote_url, vehicle_id=cfg['vehicle_id'])
 
-    #Get all autopilot signals from remote host
-    mypilot = dk.remotes.RemoteClient(remote_url, vehicle_id=cfg['vehicle_id'])
+    #setup a local pilot
+    mypilot = dk.pilots.KerasCategorical(model_path=cfg['pilot_model_path'])
+    mypilot.load()
 
     #Create your car
     car = dk.vehicles.BaseVehicle(drive_loop_delay=cfg['vehicle_loop_delay'],
                                   camera=mycamera,
                                   actuator_mixer=dd,
+                                  remote=myremote,
                                   pilot=mypilot)
+    
+    #Start the drive loop
     car.start()
